62fd925fdb50e18e5b38ceb5fb749af9c2573d93
DMozil <opencrosspost@gmail.com>
2016-03-28 22:51:42 +0300

    first commit

---
+++ Project/BusinessCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class BusinessCard
+    {
+        public void D()
+        {
+            Console.WriteLine("Hello, it`s function D() from class BusinessCard");
+        }
+    }
+}
