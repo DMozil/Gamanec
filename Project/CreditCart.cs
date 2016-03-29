6511bfce6a7878318609956dd374bdf1b1fa47a4
Lyubomyr Holyak <lubomir.lviv@gmail.com>
2016-03-29 15:54:38 +0300

    Add Class CreditCart from project Gamanec created by DMozil

---
+++ Project/CreditCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class CreditCart
+    {
+        private string hello;
+
+        public CreditCart()
+        {
+            hello = "Hello, Hi this method Show from class CreditCart";
+        }
+        public void Show()
+        {
+            Console.WriteLine(hello);
+        }
+    }
+}
